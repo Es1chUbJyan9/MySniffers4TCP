264cf9041d73d4d78221bc8924db5e9f6b469408
Es1chUb.Jyan9 <Es1chUb.Jyan9@gmail.com>
2019-04-21 04:34:28 +0800

    V1.0

--- MySniffers4TCP.cpp
+++ MySniffers4TCP.cpp
@@ -3,9 +3,11 @@
 #include <cstring>
 #include <chrono>
 #include <iostream>
-#include <netinet/ip.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/if_ether.h>
 
 #define BUFFER_SIZE 65535
 #define PATH "./data.txt"
@@ -38,18 +40,17 @@ private:
 
 class Sniffer4TCP {
 public:
-    Sniffer4TCP(char* path, unsigned int bSize):bufferSize(bSize),countLimit()
+    Sniffer4TCP(char* path, unsigned int bSize):bufferSize(bSize)
     {
         strncpy(database_path, path, 1024);
         buffer = (unsigned char *)malloc(bufferSize);
         payload = (unsigned char *)malloc(bufferSize);
 
-        sock = socket (PF_INET, SOCK_RAW, IPPROTO_TCP);
+    	sock = socket (AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
         if(sock == -1){
             cout << "Failed to create socket" << endl;
             exit(1);
         }
-
     }
 
     void StartSniffer(int time_limit, int count_limit){
@@ -63,41 +64,42 @@ public:
     }
 
     void ReadPacket(){
-        packet_size = recvfrom(sock, buffer, bufferSize, 0, NULL, NULL);
+        packetSize = recvfrom(sock, buffer, bufferSize, 0, NULL, NULL);
     }
 
     void AnalysisPacket(){
-        struct iphdr *ip_packet = (struct iphdr *)buffer;
-        packetSize = ntohs(ip_packet->tot_len);
-        headerSize = (int)ip_packet->ihl;
-        payloadSize = packetSize - headerSize;
+        struct ethhdr *eth = (struct ethhdr *)buffer;
+        printf("Ethernet Header\n");
+        printf("Source Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",eth->h_source[0],eth->h_source[1],eth->h_source[2],eth->h_source[3],eth->h_source[4],eth->h_source[5]);
+        printf("Destination Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",eth->h_dest[0],eth->h_dest[1],eth->h_dest[2],eth->h_dest[3],eth->h_dest[4],eth->h_dest[5]);
+        printf("Protocol : %d\n",eth->h_proto);
+        
+        struct iphdr *ip_packet = (struct iphdr *)(buffer + sizeof(struct ethhdr));
+        headerSize = ip_packet->ihl*4;
+        payloadSize = packetSize - (ip_packet->ihl*4 + sizeof(struct ethhdr) + sizeof(struct iphdr));
         packetId = ntohs(ip_packet->id);
-
-        struct sockaddr_in source_socket_address, dest_socket_address;
         memset(&source_socket_address, 0, sizeof(source_socket_address));
         source_socket_address.sin_addr.s_addr = ip_packet->saddr;
         memset(&dest_socket_address, 0, sizeof(dest_socket_address));
         dest_socket_address.sin_addr.s_addr = ip_packet->daddr;
-
-
         printf("Incoming Packet ID: %d\n", packetId);
         printf("Packet Size (bytes): %d\n",packetSize);
         printf("Header Size (bytes): %d\n",headerSize);
         printf("Payload Size (bytes): %d\n", payloadSize);
         printf("Source Address: %s\n", (char *)inet_ntoa(source_socket_address.sin_addr));
         printf("Destination Address: %s\n", (char *)inet_ntoa(dest_socket_address.sin_addr));
-
-        for (int i=0; i < packetSize; i++){
-            printf(" %02x",buffer[i]);
-            if ((i + 1) % 16 == 0){
-                printf("\n");
-            }
-        }
-        printf("\n\n");  
+        unsigned char *data = (buffer + ip_packet->ihl*4 + sizeof(struct ethhdr) + sizeof(struct iphdr));
         
+        printf("Payload:\n");
         for(int i=0; i<payloadSize; i++){
-            payload[i] = buffer[i+headerSize];
+            payload[i] = data[i];
+
+            printf(" %02X",payload[i]);
+            if ((i + 1) % 16 == 0)
+                printf("\n");
         }
+        printf("\n");
+
     }
 
     void RecordPacket(){
@@ -107,9 +109,8 @@ public:
             exit(1);
         }
 
-        fprintf(database, "Packet_ID %u:", packetId);
         for(int i=0; i<payloadSize ;i++){
-            fprintf(database, " %02x", payload[i]);
+            fprintf(database, " %02X", payload[i]);
         }
         fprintf(database, "\n");
         
@@ -118,7 +119,6 @@ public:
 
 private:
     struct sockaddr_in source_socket_address, dest_socket_address;    
-    int packet_size;
     unsigned char *buffer;
     unsigned char *payload;
     unsigned int bufferSize;
@@ -126,7 +126,7 @@ private:
     unsigned int headerSize;
     unsigned int payloadSize;
     unsigned int packetId;
-    unsigned int countLimit;
+    unsigned int count_limit;
     int sock;
     char database_path[1024];
     FILE *database;
@@ -143,7 +143,6 @@ int main(int argc, char *argv[]){
 
     Sniffer4TCP sniffer(PATH, BUFFER_SIZE);
     sniffer.StartSniffer(atoi(argv[1]), atoi(argv[2]));
-    
 
     return 0;
 }
--- Testbench.py
+++ Testbench.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python
 import subprocess, os, sys
 
-dns_bash_command = 'nslookup -vc google.com 8.8.8.8'
-sniffer_bash_command = './MySniffers4TCP.out 10 5'
+# 使用scapy 驗證
+from scapy.all import *
 
+# 啟動監聽器
+sniffer_bash_command = './MySniffers4TCP 10 5'
 sniffer_proc = subprocess.Popen(sniffer_bash_command, shell=True)
-dns_proc = subprocess.Popen(dns_bash_command, shell=True)
+
+# 發送測試訊息
+payload = "Hello, World!"
+test_packet = IP(src="172.217.160.110", dst="172.217.160.110") / TCP(sport=9999, dport=80) / payload
+send(test_packet)
+send(test_packet)
+send(test_packet)
+send(test_packet)
+send(test_packet)
 
 sniffer_proc.wait()
 
+# hexdump of payload
+# 0000  48 65 6C 6C 6F 2C 20 57 6F 72 6C 64 21           Hello, World!
+ans = '48 65 6C 6C 6F 2C 20 57 6F 72 6C 64 21'
 
-print ('Testing...')
+# 比對紀錄
+flag = False
+print ('\nTesting...')
 with open( './data.txt', mode='r') as data:
     for line in data:
-        print line
-
-
-print ('Test Over')
+        if line.strip() == ans:
+            flag = True
+if flag:
+    print('Test Pass')
+else:
+    print('Test False')
